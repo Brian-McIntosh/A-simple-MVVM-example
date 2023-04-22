d665fec6d948aa7d3aee680f32943e3c6edb3f72
Brian McIntosh <brian@mixinteractive.com>
2023-04-22 15:24:41 -0400

    Initial Commit

---
+++ MVVM1/MVVM1App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM1App.swift
+//  MVVM1
+//
+//  Created by Brian McIntosh on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
